170bee57d098d134c7a3855c40c325d73dc8717f
=DIOGOODELLI <=diogodelli@gmail.com>
2022-06-28 16:57:21 -0300

    commit correção primeiro click

--- gastos/src/main/java/com/diogo/gastos/config/ConfigureJSF.java
+++ gastos/src/main/java/com/diogo/gastos/config/ConfigureJSF.java
@@ -6,6 +6,8 @@ import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.primefaces.webapp.filter.FileUploadFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +16,14 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+
 @Configuration
 public class ConfigureJSF implements ServletContextInitializer {
 
 	private static final String LANGUAGE = "language";
 	private static final String XHTML = "*.xhtml";
 	private static final String FACES_SERVLET = "FacesServlet";
+	private static final String PRIME_FACES_FILE_UPLOAD_FILTER = "PrimeFaces FileUpload Filter";
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
@@ -28,9 +32,16 @@ public class ConfigureJSF implements ServletContextInitializer {
 		servletContext.setInitParameter("javax.faces.STATE_SAVING_METHOD", "server");
 		servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Production");
 		servletContext.setInitParameter("javax.faces.FACELETS_REFRESH_PERIOD", "1");
-		servletContext.setInitParameter("javax.faces.STATE_SAVING_METHOD", "client");
-		servletContext.setInitParameter("com.sun.faces.enableRestoreView11Compatibility", "true");
-		servletContext.setInitParameter("org.apache.myfaces.NUMBER_OF_VIEWS_IN_SESSION", "100");
+		servletContext.setInitParameter("primefaces.FONT_AWESOME", "true");
+		servletContext.setInitParameter("primefaces.UPLOADER", "commons");
+	}
+
+	@Bean
+	public FilterRegistrationBean<FileUploadFilter> FileUploadFilter() {
+		FilterRegistrationBean<FileUploadFilter> registration = new FilterRegistrationBean<FileUploadFilter>();
+		registration.setFilter(new org.primefaces.webapp.filter.FileUploadFilter());
+		registration.setName(PRIME_FACES_FILE_UPLOAD_FILTER);
+		return registration;
 	}
 
 	@Bean
@@ -45,7 +56,7 @@ public class ConfigureJSF implements ServletContextInitializer {
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
+		slr.setDefaultLocale(Locale.getDefault());
 		return slr;
 	}
 
